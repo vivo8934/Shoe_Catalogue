7f867a2a3bb225503c77249fe758ec44a3bc9449
vivo8934 <mfundo8934@gmail.com>
2017-04-11 12:27:01 +0200

    github

---
+++ Admin.js
@@ -0,0 +1,9 @@
+var name = document.querySelector('.name');
+var password = document.getElementsByName('.psw')
+var btn = document.getElementsByName('.btn')
+
+btn.addEventListener('click',  function(){
+
+alert("ola")
+
+});
