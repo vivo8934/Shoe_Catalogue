485ee78d51371be6941722f00c37a3d92205f19e
vivo8934 <mfundo8934@gmail.com>
2017-04-24 14:47:46 +0200

    template

--- Shoe_Catalogue.js
+++ Shoe_Catalogue.js
@@ -53,26 +53,75 @@
 
 var MyTemp = document.querySelector('.Template');
 var search = document.querySelector('.myBtn');
-var color = document.querySelector('.color');
+var allbtn = document.querySelector('.allbtn');
 var size = document.querySelector('.DSize');
 var Mycolor = document.querySelector('.myColor');
+var myBrand = document.querySelector('.Dbrand');
+var brand = document.querySelector('.brand');
+var tempDrop = document.querySelector('.TemplateDrop');
+var SizeDrop = document.querySelector('.SizeDrop');
 
 var TempInstance = Handlebars.compile(MyTemp.innerHTML);
+
+var tempDropInstance = Handlebars.compile(tempDrop.innerHTML);
+
+var SizeDropInstance = Handlebars.compile(tempDrop.innerHTML);
+//size.innerHTML = results;
+
 var myshoes=document.querySelector(".myshoes");
 
-search.addEventListener('click',  function(){
-  var colorChoosed = Mycolor.value;
-  var sizeChoosed = size.value;
-  var shoesL = [];
+var colorA = uniqColor(shoes);
+var colorF = function(){
+  Mycolor.innerHTML = tempDropInstance({shoes:colorA});
+}
+colorF();
 
+var SizeA = uniqSize(shoes);
+var SizeF = function(){
+size.innerHTML = SizeDropInstance({shoes: SizeA});
+}
+SizeF();
+
+function uniqColor(){
+var colors = [];
+var colorMap = {};
+
+for(var i=0; i<shoes.length; i++){
+  var shoe = shoes[i]
+  if(colorMap[shoe.color] === undefined){
+    colorMap[shoe.color] = shoe.color;
+    colors.push(shoe.color);
+  }
+}
+return colors;
+};
+
+function uniqSize(){
+var sizeMap = {};
+var size = [];
+for(var i=0; i<shoes.length; i++){
+  var shoe = shoes[i]
+  if(sizeMap[shoe.size] === undefined){
+    sizeMap[shoe.size] = shoe.size;
+    size.push(shoe.size);
+  }
+}
+return size;
+};
+
+var myshoes=document.querySelector(".myshoes");
+
+allbtn.addEventListener('click',  function(){
+
+  var shoesL = [];
   for (var i = 0; i < shoes.length; i++) {
+
     var sh = shoes[i];
-    var colorMatches = (sh.color == colorChoosed || colorChoosed == 'all');
-    var sizeMatches = (sh.size == sizeChoosed || sizeChoosed == 'all');
-    if(colorMatches && sizeMatches){
-      shoesL.push(sh);
-    }
+
+    var colorM = sh.color
+    shoesL.push(sh);
     myshoes.innerHTML = TempInstance({shoes: shoesL})
+
   }
 
 });
